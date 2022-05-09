b6f629c2140d98e371dad3c8867489bdc4b97c37
EngJao89 <eng_joao@outlook.com>
2022-05-08 23:03:09 -0300

    Closes 02:"Implementa Solid e Jest."

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-const teste =1;
+import express from 'express';
+import cors from 'cors';
+import { routes } from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('HTTP server running!');
+});
